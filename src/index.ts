284b69cef4e8f27adae7df15e93579deb258ccc7
Ubeyt Demir <190541075@firat.edu.tr>
2020-09-01 11:26:47 +0300

    initial files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import cors from "cors"
+import {createServer} from "http";
+import services from "./services";
+
+
+
+//EXPRESS
+const express = require("express");
+const app  = new express();
+app.use(cors());
+app.use(express.json());
+app.use("/api",services)
+
+
+
+// SERVER
+const server = createServer(app);
+server.listen(process.env.PORT);
+server.on("listening",()=>{
+    console.log(`Server is up and running on port : ${process.env.PORT}`)
+})
